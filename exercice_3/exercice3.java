da8e939e074d1b6df83a3bcbe7b69e8185e2b152
Eliace <tacalada@3il.fr>
2024-11-26 14:56:16 +0100

    correction exercice 3

--- exercice_3/exercice3.java
+++ exercice_3/exercice3.java
@@ -1,22 +1,21 @@
 public class StockManager {
 
-    // gere le stock
+    // Gère le stock
     public void gererStock(String typeOperation, String produit, int quantite, int stock) {
-        // Ajout au stock
-        if (typeOperation.equals("ajout")) {
-            stock += quantite;
-            System.out.println("Produit : " + produit + ", Stock après ajout : " + stock);
-        // Retrait du stock
-        } else if (typeOperation.equals("retrait")) {
-            if (stock >= quantite) {
-                stock -= quantite;
-                System.out.println("Produit : " + produit + ", Stock après retrait : " + stock);
-            } else {
-                System.out.println("Stock insuffisant pour le produit : " + produit);
+        switch (typeOperation) {
+            case "ajout" -> {
+                stock += quantite;
+                System.out.println("Produit : " + produit + ", Stock après ajout : " + stock);
             }
-        // Si l'action est inconnu
-        } else {
-            System.out.println("Opération inconnue.");
+            case "retrait" -> {
+                if (stock >= quantite) {
+                    stock -= quantite;
+                    System.out.println("Produit : " + produit + ", Stock après retrait : " + stock);
+                } else {
+                    System.out.println("Stock insuffisant pour le produit : " + produit);
+                }
+            }
+            default -> System.out.println("Opération inconnue.");
         }
     }
 }
