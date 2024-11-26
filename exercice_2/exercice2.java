d44d5ee38a6934b0f0b798d7965b3dbf456cca11
Eliace <tacalada@3il.fr>
2024-11-26 14:52:58 +0100

    correction exercice 2

--- exercice_2/exercice2.java
+++ exercice_2/exercice2.java
@@ -1,21 +1,15 @@
-public class GestionnaireNotes {
+public class GestionnaireNotes { 
 
-    // Affiche note et moyennes
+    // Affiche les notes et leur moyenne
     public void afficherNotesEtMoyenne(String nomEtudiant, int[] notes) {
-        // Affichage des notes
         System.out.println("Notes de " + nomEtudiant + ":");
+        int somme = 0;
+
         for (int note : notes) {
             System.out.println("- " + note);
-        }
-
-        // Calcul de la somme des notes
-        int somme = 0;
-        for (int note : notes) {
             somme += note;
         }
 
-        // Affichage de la moyenne
-        double moyenne = (double) somme / notes.length;
-        System.out.println("Moyenne : " + moyenne);
+        System.out.println("Moyenne : " + (double) somme / notes.length);
     }
 }
